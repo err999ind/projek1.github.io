03a059f5af4242411e6585303f44a6e02ff1df8a
err999ind <xdllmk@gmail.com>
2025-11-17 12:11:13 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+
+// Config Firebase lu
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcQsZD5kOZi7uxdRFtZNbFbgigNjcAJ4c",
+  authDomain: "tempat-cerita-ef555.firebaseapp.com",
+  databaseURL: "https://tempat-cerita-ef555-default-rtdb.firebaseio.com",
+  projectId: "tempat-cerita-ef555",
+  storageBucket: "tempat-cerita-ef555.firebasestorage.app",
+  messagingSenderId: "605791578144",
+  appId: "1:605791578144:web:a7d161634ce2ee16d7b445",
+  measurementId: "G-9DEDTLQT77"
+};
